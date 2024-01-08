451ef14c840dfcf3875af2698917da2cf5acc114
Nagy Zétény <nagyzeteny6@gmail.com>
2024-01-08 19:37:17 +0100

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  printWidth: 100,
+};
+
+module.exports = config;
