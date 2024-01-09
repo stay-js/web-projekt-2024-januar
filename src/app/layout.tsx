08c389435c305c36da554335352ed287f2647444
Nagy Zétény <nagyzeteny6@gmail.com>
2024-01-09 16:39:15 +0100

    switch to system ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Viewport } from 'next';
-import { Inter } from 'next/font/google';
-import { cn } from '~/utils/cn';
 import { Navigation } from '~/components/navigation';
 import { Footer } from '~/components/footer';
 
@@ -12,16 +10,9 @@ export const viewport: Viewport = {
   colorScheme: 'light',
 };
 
-const inter = Inter({ subsets: ['latin-ext'] });
-
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <html lang="hu" className="antialiased">
-    <body
-      className={cn(
-        inter.className,
-        'grid min-h-screen grid-cols-1 grid-rows-[1fr_auto] overflow-x-hidden',
-      )}
-    >
+    <body className="grid min-h-screen grid-cols-1 grid-rows-[1fr_auto] overflow-x-hidden">
       <div>
         <Navigation />
         {children}
