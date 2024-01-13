30c9a2dc91827e11d5ba578ac2f9fc7525753304
Nagy Zétény <nagyzeteny6@gmail.com>
2024-01-13 17:05:33 +0100

    better navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -13,28 +13,33 @@ export const Navigation: React.FC = () => {
 
   const handleClose = () => {
     setIsToggled(false);
+
     document.body.style.overflowY = 'scroll';
+
+    const main = document.querySelector('main');
+    if (main) main.ariaHidden = 'false';
   };
 
   const handleToggle = () => {
     setIsToggled((value) => {
-      value
-        ? (document.body.style.overflowY = 'scroll')
-        : (document.body.style.overflowY = 'hidden');
+      document.body.style.overflowY = value ? 'scroll' : 'hidden';
+
+      const main = document.querySelector('main');
+      if (main) main.ariaHidden = value ? 'true' : 'false';
 
       return !value;
     });
   };
 
   return (
-    <nav className="flex w-full select-none items-center justify-between px-6 py-4">
+    <nav className="flex h-16 w-full select-none items-center justify-between px-6">
       <Link href="/" className="text-2xl font-bold">
         Web Projekt
       </Link>
 
       <button
         className="z-10 h-6 w-6 lg:hidden"
-        title="Menü megnyitása"
+        title={`Menü ${isToggled ? 'bezárása' : 'megnyitása'}`}
         type="button"
         onClick={handleToggle}
       >
@@ -60,23 +65,26 @@ export const Navigation: React.FC = () => {
 
       <div
         className={cn(
-          'fixed inset-0 flex items-center justify-end bg-[hsl(240,_100%,_5%,_60%)] lg:static lg:flex lg:h-fit lg:w-fit lg:bg-transparent',
+          'fixed left-0 top-16 h-[calc(100vh-4rem)] w-full overflow-y-auto bg-white lg:static lg:flex lg:h-fit lg:w-fit',
           !isToggled && 'hidden',
         )}
       >
-        <ul className="flex h-screen w-9/12 flex-col gap-6 bg-white px-6 pt-32 lg:h-fit lg:w-fit lg:flex-row lg:gap-0 lg:p-0">
-          {navItems.map(({ name, path }) => (
-            <li key={path}>
-              <a
+        <ul className="flex flex-col gap-8 px-6 pb-20 pt-4 lg:w-fit lg:flex-row lg:gap-1 lg:p-0">
+          {navItems.map(({ path, name }) => (
+            <li
+              key={path}
+              className="relative after:absolute after:-bottom-4 after:h-px after:w-full after:bg-neutral-300 lg:after:hidden"
+            >
+              <Link
                 className={cn(
-                  'text-lg text-neutral-900 transition-colors hover:text-neutral-500 lg:rounded-md lg:px-3 lg:py-2 lg:text-base lg:font-semibold lg:after:hidden lg:hover:bg-neutral-300 lg:hover:text-neutral-800',
+                  'flex items-center gap-2 font-bold text-black transition-colors lg:static lg:rounded-md lg:px-3 lg:py-2 lg:font-medium lg:hover:bg-neutral-300',
                   path !== pathname && 'lg:text-neutral-600',
                 )}
                 onClick={handleClose}
                 href={path}
               >
                 {name}
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
